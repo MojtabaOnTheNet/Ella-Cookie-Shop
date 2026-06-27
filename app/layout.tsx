678334988a0a6cc840a8d6ae468eb2587b73d0d7
Mojtaba Alizadeh <moj.dazalih@gmail.com>
2026-06-27 15:59:27 +0330

    add metadata and smooth scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,17 @@ import { Vazirmatn } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils"
+import { Metadata } from "next"
 
 const fontPersian = Vazirmatn({
   subsets: ["arabic"],
   variable: "--font-persian",
 })
+
+export const metadata: Metadata = {
+  title: "کوکی‌های الا",
+  description: "...",
+}
 
 export default function RootLayout({
   children,
@@ -18,7 +24,7 @@ export default function RootLayout({
     <html
       lang="fa"
       suppressHydrationWarning
-      className={cn("antialiased", fontPersian.className)}
+      className={cn("scroll-smooth antialiased", fontPersian.className)}
     >
       <body dir="rtl">
         <ThemeProvider>{children}</ThemeProvider>
