7b991cb03b9ee0f6adb9e4fc96c3a8b8cf656db5
Mojtaba Alizadeh <moj.dazalih@gmail.com>
2026-07-03 22:14:45 +0330

    Add cart page responsiveness for desktop

--- app/(main)/cart/page.tsx
+++ app/(main)/cart/page.tsx
@@ -42,107 +42,14 @@ const Page = () => {
         </div>
       ) : (
         <div className="relative mt-5 flex h-[calc(100dvh-60px)] flex-col items-center justify-start gap-3">
-          <h1 className="absolute -top-2 rounded-2xl bg-background px-2 text-xl font-black text-foreground">
+          <h1 className="absolute -top-2 rounded-2xl bg-background px-2 text-xl font-black text-foreground md:text-3xl">
             سبد خرید
           </h1>
-          <div className="flex h-[92%] w-[95%] flex-col items-center justify-start gap-3 rounded-2xl bg-secondary px-5 pt-10">
-            <ScrollArea className="max-h-[83%] w-full rounded-2xl" dir="rtl">
-              <div className="my-3 flex h-[15%] w-full items-center justify-start rounded-2xl bg-card p-5">
-                <Image
-                  src="/pink-donut.png"
-                  width={90}
-                  height={90}
-                  alt="An image of a donut"
-                  className="ml-2 rounded-2xl bg-muted"
-                />
-                <div className="flex flex-col gap-2">
-                  <h1 className="text-lg font-extrabold text-accent-foreground">
-                    دونات شکلاتی
-                  </h1>
-                  <h2 className="text-sm">
-                    <IconWeight className="ml-1 inline size-4 opacity-50" />
-                    {`${(25).toLocaleString("fa-IR")} گرم`}
-                  </h2>
-                  <h2 className="font-bold">{`${(800000).toLocaleString("fa-IR")} تومان`}</h2>
-                </div>
-                <div className="mr-auto flex flex-col items-center justify-center gap-2 text-2xl">
-                  <span className="text-green-500">+</span>
-                  <span>{(1).toLocaleString("fa-IR")}</span>
-                  <span className="text-red-500">-</span>
-                </div>
-              </div>
-              <div className="my-3 flex h-[15%] w-full items-center justify-start rounded-2xl bg-card p-5">
-                <Image
-                  src="/pink-donut.png"
-                  width={90}
-                  height={90}
-                  alt="An image of a donut"
-                  className="ml-2 rounded-2xl bg-muted"
-                />
-                <div className="flex flex-col gap-2">
-                  <h1 className="text-lg font-extrabold text-accent-foreground">
-                    دونات شکلاتی
-                  </h1>
-                  <h2 className="text-sm">
-                    <IconWeight className="ml-1 inline size-4 opacity-50" />
-                    {`${(25).toLocaleString("fa-IR")} گرم`}
-                  </h2>
-                  <h2 className="font-bold">{`${(800000).toLocaleString("fa-IR")} تومان`}</h2>
-                </div>
-                <div className="mr-auto flex flex-col items-center justify-center gap-2 text-2xl">
-                  <span className="text-green-500">+</span>
-                  <span>{(1).toLocaleString("fa-IR")}</span>
-                  <span className="text-red-500">-</span>
-                </div>
-              </div>
-              <div className="my-3 flex h-[15%] w-full items-center justify-start rounded-2xl bg-card p-5">
-                <Image
-                  src="/pink-donut.png"
-                  width={90}
-                  height={90}
-                  alt="An image of a donut"
-                  className="ml-2 rounded-2xl bg-muted"
-                />
-                <div className="flex flex-col gap-2">
-                  <h1 className="text-lg font-extrabold text-accent-foreground">
-                    دونات شکلاتی
-                  </h1>
-                  <h2 className="text-sm">
-                    <IconWeight className="ml-1 inline size-4 opacity-50" />
-                    {`${(25).toLocaleString("fa-IR")} گرم`}
-                  </h2>
-                  <h2 className="font-bold">{`${(800000).toLocaleString("fa-IR")} تومان`}</h2>
-                </div>
-                <div className="mr-auto flex flex-col items-center justify-center gap-2 text-2xl">
-                  <span className="text-green-500">+</span>
-                  <span>{(1).toLocaleString("fa-IR")}</span>
-                  <span className="text-red-500">-</span>
-                </div>
-              </div>
-              <div className="my-3 flex h-[15%] w-full items-center justify-start rounded-2xl bg-card p-5">
-                <Image
-                  src="/pink-donut.png"
-                  width={90}
-                  height={90}
-                  alt="An image of a donut"
-                  className="ml-2 rounded-2xl bg-muted"
-                />
-                <div className="flex flex-col gap-2">
-                  <h1 className="text-lg font-extrabold text-accent-foreground">
-                    دونات شکلاتی
-                  </h1>
-                  <h2 className="text-sm">
-                    <IconWeight className="ml-1 inline size-4 opacity-50" />
-                    {`${(25).toLocaleString("fa-IR")} گرم`}
-                  </h2>
-                  <h2 className="font-bold">{`${(800000).toLocaleString("fa-IR")} تومان`}</h2>
-                </div>
-                <div className="mr-auto flex flex-col items-center justify-center gap-2 text-2xl">
-                  <span className="text-green-500">+</span>
-                  <span>{(1).toLocaleString("fa-IR")}</span>
-                  <span className="text-red-500">-</span>
-                </div>
-              </div>
+          <div className="flex h-[92%] w-[95%] flex-col items-center justify-start gap-3 rounded-2xl bg-secondary px-5 pt-10 md:h-[90%] md:w-[70%] md:justify-center md:bg-background xl:w-[50%]">
+            <ScrollArea
+              className="max-h-[83%] w-full rounded-2xl md:h-full md:bg-secondary md:p-3"
+              dir="rtl"
+            >
               <div className="my-3 flex h-[15%] w-full items-center justify-start rounded-2xl bg-card p-5">
                 <Image
                   src="/pink-donut.png"
@@ -192,7 +99,7 @@ const Page = () => {
                 </div>
               </div>
             </ScrollArea>
-            <div className="mt-auto mb-3 flex w-full items-center justify-between gap-1 border-t-3 border-muted p-3">
+            <div className="mt-auto mb-3 flex w-full items-center justify-between gap-1 border-t-3 border-muted p-3 md:mt-0 md:mb-0 md:rounded-2xl md:border-0 md:bg-secondary">
               <div className="flex flex-col items-start justify-center gap-1">
                 <span className="text-sm text-foreground/50">جمع سبد خرید</span>
                 <span>{`${(2600000).toLocaleString("fa-IR")} تومان`}</span>
