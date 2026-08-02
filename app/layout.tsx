a1d1650e81568929288def8142647e1473430d0b
Mojtaba Alizadeh <moj.dazalih@gmail.com>
2026-08-02 19:46:26 +0330

    block robots from crawling the site

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,10 @@ const fontPersian = Vazirmatn({
 export const metadata: Metadata = {
   title: "کوکی‌های الا",
   description: "...",
+  robots: {
+    index: false,
+    follow: false,
+  },
 }
 
 export default function RootLayout({
